735fc9827cc0ace4b8d866bef1597b8c29b2d0af
VeronGoggans <j.s.v.goggans@gmail.com>
2024-11-21 13:48:51 +0100

    init

---
+++ utils.py
@@ -0,0 +1,22 @@
+def add_snippet():
+    pass
+
+
+def list_snippets():
+    pass
+
+
+def search_snippets():
+    pass
+
+
+def delete_snippet():
+    pass
+
+
+def update_snippet():
+    pass
+
+
+def add_tag():
+    pass
